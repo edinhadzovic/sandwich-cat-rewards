0fe492fefe96e0f886efd2a2c186d7f97533b28b
Edin Hadzovic <contact@edinhadzovic.com>
2024-08-01 21:16:51 +0200

    initial commit

---
+++ src/wagmi.ts
@@ -0,0 +1,21 @@
+import { http, createConfig } from 'wagmi'
+import { base } from 'wagmi/chains'
+import { coinbaseWallet, injected, walletConnect } from 'wagmi/connectors'
+
+export const config = createConfig({
+  chains: [base],
+  connectors: [
+    injected(),
+    coinbaseWallet(),
+    walletConnect({ projectId: import.meta.env.VITE_WC_PROJECT_ID }),
+  ],
+  transports: {
+    [base.id]: http(),
+  },
+})
+
+declare module 'wagmi' {
+  interface Register {
+    config: typeof config
+  }
+}
